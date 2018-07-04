3811c0c1b0b0c7338e5ed2516c3fc97e7442c518
LimJoongSub <quhb2455@gmail.com>
2018-07-04 17:34:12 +0900

    .
    .

--- 임중섭/Lim/module/dbConnect.php
+++ 임중섭/Lim/module/dbConnect.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
     <?php
     $conn = mysqli_connect("localhost","root","misozium23");
     $sqli = mysqli_select_db($conn, "test");
 
      ?>
-
-  </body>
-</html>
--- 임중섭/Lim/test.php
+++ 임중섭/Lim/test.php
@@ -10,12 +10,12 @@
 
       $d = "오전";
       echo date("9999-12-31 $d 12:00:00");
-      // $sql = "SELECT pw_salt FROM login WHERE id = '$_POST[LOG_id]' ";
-      //
-      // $result = $conn->query($sql);
-      // $row = $result->fetch_array(MYSQLI_ASSOC);
-      //
-      // echo $row[pw_salt];
+      $sql = "SELECT pw_salt FROM login WHERE id = '$_POST[LOG_id]' ";
+
+      $result = $conn->query($sql);
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+
+      echo $row[pw_salt];
 
       mysqli_close();
     ?>
