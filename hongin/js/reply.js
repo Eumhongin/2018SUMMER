2456d867d390ffe000f3ce49c5be9f0fc5ec042d
Eumhongin <zizon0wpxm@naver.com>
2018-07-09 17:51:23 +0900

    ''

--- hongin/js/reply.js
+++ hongin/js/reply.js
@@ -1,28 +1,28 @@
 
 $(document).ready(function(){
 
-  $('.reply').on('click',function(){
+  $('.box').on('click',function(){
     document.getElementsByClassName("forlogin")[0].classList.remove("hide");
-    document.getElementsByClassName("loginpop")[0].classList.remove("hide");
+    document.getElementsByClassName("click_list")[0].classList.remove("hide");
     $('.forlogin').animate({opacity:'1'},300);
-    $('.loginpop').animate({opacity:'1'},300);
+    $('.click_list').animate({opacity:'1'},300);
   });
   $(document).keyup(function(e) {
     //if (e.keyCode === 13) $('.save').click();     // enter
     if (e.keyCode === 27&&!(document.getElementsByClassName("forlogin")[0].classList.contains("hide")))
     {
         document.getElementsByClassName("forlogin")[0].classList.add("hide");
-        document.getElementsByClassName("loginpop")[0].classList.add("hide");
+        document.getElementsByClassName("click_list")[0].classList.add("hide");
         $('.forlogin').css("opacity","0");
-        $('.loginpop').css("opacity","0");
+        $('.click_list').css("opacity","0");
     }
 
   });
   $('.forlogin').on('click',function(){
     document.getElementsByClassName("forlogin")[0].classList.add("hide");
-    document.getElementsByClassName("loginpop")[0].classList.add("hide");
+    document.getElementsByClassName("click_list")[0].classList.add("hide");
     $('.forlogin').css("opacity","0");
-    $('.loginpop').css("opacity","0");
+    $('.click_list').css("opacity","0");
 
 
   });
